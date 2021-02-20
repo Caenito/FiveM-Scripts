167fb10b96f8108e57b97b069c43c44516f4af96
Caen <79376426+Caenito@users.noreply.github.com>
2021-02-20 20:37:27 +0200

    Add files via upload

---
+++ afkkick/server.lua
@@ -0,0 +1,4 @@
+RegisterServerEvent("kickForBeingAnAFKDouchebag")
+AddEventHandler("kickForBeingAnAFKDouchebag", function()
+	DropPlayer(source, "[XENONRO] Ai primit deoarece ai stat prea mult AFK.")
+end)
